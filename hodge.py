e1d41a2986000890ee7dfa0e35b9779408cbb832
Sahir Sujahudeen <m.s.sujahudeen@student.tudelft.nl>
2024-04-26 14:05:50 +0200

    changes to hodge

--- hodge.py
+++ hodge.py
@@ -4,55 +4,20 @@ import scipy.sparse as sparse
 import numpy as np
 
 
-# L = float(1.0)
-# Re = float(1000)    # Reynolds number 
-# N = 10		# mesh cells in x- and y-direction
 
-# u = np.zeros((2*N*(N+1),1), dtype = np.float64)
-# p = np.zeros((N*N+4*N,1), dtype = np.float64)
-# tx = np.zeros((N+1,1), dtype = np.float64)     # grid points on primal grid
-# x = np.zeros((N+2,1), dtype = np.float64)      # grid points on dual grid
-# th = np.zeros((N), dtype = np.float64)       # mesh width primal grid
-# h = np.zeros((N+1), dtype = np.float64)      # mesh width dual grid 
-
-# #Generation of a non-uniform grid
-# x[0] = 0
-# x[N+1] = 1
-# for i in range(N+1):
-#     xi = i*L/N
-#     tx[i] = 0.5*(1. - np.cos(np.pi*xi))     #  tx mesh point for primal mesh
-#     if i>0:
-#         th[i-1] = tx[i] - tx[i-1]           # th mesh width on primal mesh
-#         x[i] = 0.5*(tx[i-1]+tx[i])          # x mesh points for dual mesh
-        
-# for i in range(N+1):
-#     h[i] = x[i+1]-x[i]                      # h mesh width on dual mesh
-
-# print("primal widths")
-# print(th)
-# print()
-# print("dual widths")
-# print(h)
-
-# coord_stack_h = np.cumsum(np.insert(h,0,0))
-# coord_stack_th = np.cumsum(np.insert(th,0,0))
-
-# X_h, Y_h = np.meshgrid(coord_stack_h,coord_stack_h)
-
-# X_th, Y_th = np.meshgrid(coord_stack_th,coord_stack_th)
-
-
-def get_Ht02(primal_width_1d, N):
-    cell_As = np.zeros(N*N)
+def get_Ht02(dual_width_1d, N):
+    Np1 = N+1
+    cell_As = np.zeros(Np1*Np1)
 
     c = 0
-    for j in primal_width_1d:
-        for i in primal_width_1d:
+    for j in dual_width_1d:
+        for i in dual_width_1d:
             cell_As[c] = i * j
             
             c+=1
     H0t2 = sparse.diags(cell_As,format='csc')
-    return splinalg.inv(H0t2)
+
+    return splinalg.inv(H0t2), cell_As
 
 
 def get_H1t1(primal_width_1d, dual_width_1d, N):
@@ -63,19 +28,61 @@ def get_H1t1(primal_width_1d, dual_width_1d, N):
 
     return sparse.diags(e_ratio,format='csc')
 
-# plt.pcolormesh(X_th, Y_th,cell_As.reshape(N, N), edgecolors='k', linewidths=0.2)
+def main():
 
-# plt.pcolormesh(X_h, Y_h,C_h, edgecolors='k', linewidths=2, cmap = 'binary')
+    import matplotlib.pyplot as plt
 
-# plt.legend()
+    L = float(1.0)
+    N = 10		# mesh cells in x- and y-direction
+
+    tx = np.zeros((N+1,1), dtype = np.float64)     # grid points on primal grid
+    x = np.zeros((N+2,1), dtype = np.float64)      # grid points on dual grid
+    th = np.zeros((N), dtype = np.float64)       # mesh width primal grid
+    h = np.zeros((N+1), dtype = np.float64)      # mesh width dual grid 
+
+    #Generation of a non-uniform grid
+    x[0] = 0
+    x[N+1] = 1
+    for i in range(N+1):
+        xi = i*L/N
+        tx[i] = 0.5*(1. - np.cos(np.pi*xi))     #  tx mesh point for primal mesh
+        if i>0:
+            th[i-1] = tx[i] - tx[i-1]           # th mesh width on primal mesh
+            x[i] = 0.5*(tx[i-1]+tx[i])          # x mesh points for dual mesh
+            
+    for i in range(N+1):
+        h[i] = x[i+1]-x[i]                      # h mesh width on dual mesh
+
+    print("primal widths")
+    print(th)
+    print()
+    print("dual widths")
+    print(h)
+
+    coord_stack_h = np.cumsum(np.insert(h,0,0))
+    coord_stack_th = np.cumsum(np.insert(th,0,0))
+
+    X_h, Y_h = np.meshgrid(coord_stack_h,coord_stack_h)
+
+    X_th, Y_th = np.meshgrid(coord_stack_th,coord_stack_th)
 
 
-# plt.show()
+    H0t2, cell_As = get_Ht02(h,N)
+    H1t1 = get_H1t1(th, h, N)
 
-# plt.imshow(H0t2.todense())
-# plt.colorbar()
-# plt.show()
+    plt.pcolormesh(X_h, Y_h,cell_As.reshape(N+1, N+1), edgecolors='k', linewidths=0.2)
 
-# plt.imshow(H1t1.todense())
-# plt.colorbar()
-# plt.show()
+    # plt.legend()
+
+    plt.show()
+
+    plt.imshow(H0t2.todense())
+    plt.colorbar()
+    plt.show()
+
+    plt.imshow(H1t1.todense())
+    plt.colorbar()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
